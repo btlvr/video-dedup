e16a1effde8daef402d20e0fa96e9f9a6f6b36d8
btlvr <you@example.com>
2020-07-14 16:36:37 -0700

    separate file input

---
+++ file_list.py
@@ -0,0 +1,19 @@
+from video import Video
+from args import args
+import os
+
+def list_files(dir):
+	for dirpath, subdirs, Videos in os.walk(dir):
+		for Video in Videos:
+			yield dirpath+'/'+Video
+
+videos = set()
+for folder in args.dirs:
+	videos = videos.union(set(map(Video, list_files(folder))))
+
+def file_is_excluded(file):
+	if not file.is_video:
+		return True
+	return False
+
+videos = [v for v in videos if not file_is_excluded(v)]
--- video-dedup.py
+++ video-dedup.py
@@ -5,26 +5,10 @@ import os
 import time
 import numpy
 
-from video import Video
 from duplicate_pools import DuplicatePools
 import logger
 from args import args
-
-def list_files(dir):
-	for dirpath, subdirs, Videos in os.walk(dir):
-		for Video in Videos:
-			yield dirpath+'/'+Video
-
-videos = set()
-for folder in args.dirs:
-	videos = videos.union(set(map(Video, list_files(folder))))
-
-def file_is_excluded(file):
-	if not file.is_video:
-		return True
-	return False
-
-videos = [v for v in videos if not file_is_excluded(v)]
+from file_list import videos
 
 pools = DuplicatePools(videos)
 
