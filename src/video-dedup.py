8356fe77664a080f91fc28a048d1a723667a70cd
btlvr <you@example.com>
2020-07-15 00:44:51 -0700

    clean up expand()

--- src/video-dedup.py
+++ src/video-dedup.py
@@ -13,15 +13,20 @@ pools = DuplicatePools(videos)
 
 if args.duration_threshold:
 	logger.excluding_by_duration(args.duration_threshold)
-	pools.expand(lambda v : v.duration(), lambda a, b : abs(a-b) <= args.duration_threshold)
+	pools.expand(
+		lambda v : v.duration(),
+		lambda a, b : abs(a-b) <= args.duration_threshold
+	)
 	logger.found_n_videos(len(pools))
 else:
 	logger.warn_duration_threshold()
 
 for timestamp in args.hashes:
 	logger.comparing_hashes_at(timestamp)
-	pools.expand(lambda v : v.hash_at(timestamp), lambda a, b : abs(numpy.mean(a-b) <= args.hash_threshold))
+	pools.expand(
+		lambda v : v.hash_at(timestamp),
+		lambda a, b : abs(numpy.mean(a-b) <= args.hash_threshold)
+	)
 	logger.n_videos_remaining(len(pools))
 
 pools.print()
-
